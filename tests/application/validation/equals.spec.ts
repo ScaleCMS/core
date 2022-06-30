507b160a245b2380f0721d3fd10652eeceea577f
Victor Guedes <victorfelipe.guedes@hotmail.com>
2022-06-30 14:49:48 -0300

    test: ensure EqualsValidator returns undefined if value is equal

--- tests/application/validation/equals.spec.ts
+++ tests/application/validation/equals.spec.ts
@@ -1,5 +1,7 @@
 import { InvalidFieldError } from '@/application/errors'
 
+import validator from 'validator'
+
 class EqualsValidator {
   constructor (
     private readonly value: string,
@@ -8,16 +10,26 @@ class EqualsValidator {
   ) {}
 
   validate (): Error | undefined {
-    return new InvalidFieldError(this.fieldName)
+    if (!validator.equals(this.value, this.compareValue)) {
+      return new InvalidFieldError(this.fieldName)
+    }
   }
 }
 
 describe('EqualsValidator', () => {
   it('should return InvalidFieldError if value is not equal', () => {
-    const sut = new EqualsValidator('any_value', 'value', 'any_field')
+    const sut = new EqualsValidator('valid_value', 'invalid_value', 'any_field')
 
     const error = sut.validate()
 
     expect(error).toEqual(new InvalidFieldError('any_field'))
+  })
+
+  it('should return undefined if value is equal', () => {
+    const sut = new EqualsValidator('any_value', 'any_value', 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
   })
 })
