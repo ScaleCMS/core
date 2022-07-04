3fcde26e81ea266ff1136e78c9f7cacd604af00e
Victor Guedes <victorfelipe.guedes@hotmail.com>
2022-07-04 17:39:37 -0300

    refactor: isolate SUT

--- tests/data/services/save-account.spec.ts
+++ tests/data/services/save-account.spec.ts
@@ -2,35 +2,45 @@ import { SaveAccountService } from '@/data/services'
 import { LoadUserAccount, SaveUserAccount } from '@/data/contracts/repos'
 import { EmailInUseError } from '@/domain/errors'
 
-import { mock } from 'jest-mock-extended'
+import { MockProxy, mock } from 'jest-mock-extended'
 
 describe('SaveAccountService', () => {
+  let userRepository: MockProxy<LoadUserAccount & SaveUserAccount>
+  let sut: SaveAccountService
+  let name: string
+  let email: string
+  let password: string
+
+  beforeAll(() => {
+    name = 'any_name'
+    email = 'any_email@mail.com'
+    password = 'any_password'
+  })
+
+  beforeEach(() => {
+    userRepository = mock()
+    sut = new SaveAccountService(userRepository)
+  })
+
   it('should call LoadUserAccount with correct params', async () => {
-    const userRepository = mock<LoadUserAccount & SaveUserAccount>()
-    const sut = new SaveAccountService(userRepository)
+    await sut.perform({ name, email, password })
 
-    await sut.perform({ name: 'any_name', email: 'any_email@mail.com', password: 'any_password' })
-
-    expect(userRepository.load).toBeCalledWith({ email: 'any_email@mail.com' })
+    expect(userRepository.load).toBeCalledWith({ email })
     expect(userRepository.load).toBeCalledTimes(1)
   })
 
   it('should return EmailInUseError when LoadUserAccount returns data', async () => {
-    const userRepository = mock<LoadUserAccount & SaveUserAccount>()
     userRepository.load.mockResolvedValueOnce({ id: 'any_id' })
-    const sut = new SaveAccountService(userRepository)
 
-    const result = await sut.perform({ name: 'any_name', email: 'any_email@mail.com', password: 'any_password' })
+    const result = await sut.perform({ name, email, password })
 
     expect(result).toEqual(new EmailInUseError())
   })
 
   it('should call SaveUserAccount when LoadUserAccount returns undefined', async () => {
-    const userRepository = mock<SaveUserAccount & LoadUserAccount>()
     userRepository.load.mockResolvedValueOnce(undefined)
-    const sut = new SaveAccountService(userRepository)
 
-    await sut.perform({ name: 'any_name', email: 'any_email@mail.com', password: 'any_password' })
+    await sut.perform({ name, email, password })
 
     expect(userRepository.save).toBeCalledWith({
       name: 'any_name',
