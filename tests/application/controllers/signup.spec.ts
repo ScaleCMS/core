a1813012358464f2153a9c0320417ae7f0abf9a0
Victor Guedes <victorfelipe.guedes@hotmail.com>
2022-07-04 15:29:40 -0300

    test: ensure SignUpController returns correct responses in different situations

--- tests/application/controllers/signup.spec.ts
+++ tests/application/controllers/signup.spec.ts
@@ -1,6 +1,8 @@
-import { SignUpController } from '@/application/controllers'
+import { Controller, SignUpController } from '@/application/controllers'
 import { EmailValidator, EqualsValidator, RequiredStringValidator } from '@/application/validation'
+import { EmailInUseError } from '@/domain/errors'
 import { CreateAccount } from '@/domain/use-cases'
+import { ServerError } from '@/application/errors'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 
@@ -14,6 +16,7 @@ describe('SignupController', () => {
 
   beforeAll(() => {
     createAccount = mock()
+    createAccount.perform.mockResolvedValue({ id: 'any_id' })
     name = 'any_name'
     email = 'any_email@mail.com'
     password = 'any_password'
@@ -22,6 +25,10 @@ describe('SignupController', () => {
 
   beforeEach(() => {
     sut = new SignUpController(createAccount)
+  })
+
+  it('should extend Controller', async () => {
+    expect(sut).toBeInstanceOf(Controller)
   })
 
   it('should build validators correctly', async () => {
@@ -42,5 +49,39 @@ describe('SignupController', () => {
 
     expect(createAccount.perform).toBeCalledWith({ name, email, password, passwordConfirmation })
     expect(createAccount.perform).toBeCalledTimes(1)
+  })
+
+  it('should return 400 if email is already in use', async () => {
+    createAccount.perform.mockResolvedValueOnce(new EmailInUseError())
+
+    const httpResponse = await sut.handle({ name, email, password, passwordConfirmation })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      body: new EmailInUseError()
+    })
+  })
+
+  it('should return 500 on infra error', async () => {
+    const error = new Error('infra_error')
+    createAccount.perform.mockRejectedValueOnce(error)
+
+    const httpResponse = await sut.handle({ name, email, password, passwordConfirmation })
+
+    expect(httpResponse).toEqual({
+      statusCode: 500,
+      body: new ServerError(error)
+    })
+  })
+
+  it('should return 200 if account is created', async () => {
+    const httpResponse = await sut.handle({ name, email, password, passwordConfirmation })
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        id: 'any_id'
+      }
+    })
   })
 })
