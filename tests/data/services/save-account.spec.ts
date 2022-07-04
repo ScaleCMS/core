7bf818de22940d0de6d06a5f298b22e93b05d94d
Victor Guedes <victorfelipe.guedes@hotmail.com>
2022-07-04 17:23:02 -0300

    test: ensure SaveAccountService returns EmailInUseError when LoadUserAccount returns data

--- tests/data/services/save-account.spec.ts
+++ tests/data/services/save-account.spec.ts
@@ -1,4 +1,5 @@
 import { LoadUserAccount } from '@/data/contracts/repos'
+import { EmailInUseError } from '@/domain/errors'
 import { SaveAccount } from '@/domain/features'
 
 class SaveAccountService {
@@ -6,8 +7,9 @@ class SaveAccountService {
     private readonly userRepository: LoadUserAccount
   ) {}
 
-  async perform ({ email }: SaveAccount.Params): Promise<void> {
+  async perform ({ email }: SaveAccount.Params): Promise<EmailInUseError> {
     await this.userRepository.load({ email })
+    return new EmailInUseError()
   }
 }
 
@@ -22,5 +24,17 @@ describe('SaveAccountService', () => {
 
     expect(userRepository.load).toBeCalledWith({ email: 'any_email@mail.com' })
     expect(userRepository.load).toBeCalledTimes(1)
+  })
+
+  it('should return EmailInUseError when LoadUserAccount returns data', async () => {
+    const userRepository = {
+      load: jest.fn()
+    }
+    userRepository.load.mockResolvedValue({ id: 'any_id' })
+    const sut = new SaveAccountService(userRepository)
+
+    const result = await sut.perform({ name: 'any_name', email: 'any_email@mail.com', password: 'any_password' })
+
+    expect(result).toEqual(new EmailInUseError())
   })
 })
