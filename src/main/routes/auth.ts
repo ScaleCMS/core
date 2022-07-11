80f0d201897ae2c44eb947eebac43bfd6c3b01da
Victor Guedes <victorfelipe.guedes@hotmail.com>
2022-07-11 19:20:50 -0300

    feat: transform ExpressRouter to a function

--- src/main/routes/auth.ts
+++ src/main/routes/auth.ts
@@ -1,7 +1,8 @@
+import { adaptExpressRoute as adapt } from '@/infra/http'
+import { makeSignUpController } from '@/main/factories/controllers'
+
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/api/auth/signup', (req, res) => {
-    res.send({ data: 'any_data' })
-  })
+  router.post('/api/auth/signup', adapt(makeSignUpController()))
 }
